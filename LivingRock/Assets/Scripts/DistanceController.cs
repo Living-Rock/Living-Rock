47c1266c1094142bccca25fac51438147b6a1f74
VoHeLi <dahrusted@gmail.com>
2022-11-19 18:23:27 +0100

    leger changement

--- LivingRock/Assets/Scripts/DistanceController.cs
+++ LivingRock/Assets/Scripts/DistanceController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DistanceController : MonoBehaviour
 {
+    [SerializeField] private PlayerInput playerInput;
     [SerializeField] private Transform crystal;
     [SerializeField] private float dieDistance = 15f;
     [SerializeField] private float recallPlateDieDistanceScale = 2f;
@@ -10,7 +12,20 @@ public class DistanceController : MonoBehaviour
     [SerializeField] private float[] visionLossSteps = { 5f, 10f, 15f };
     [SerializeField] private float[] visionLossScales = { 1f, .5f, .33f };
 
-    [HideInInspector] public bool isOnRecallPlate = false;
+    [SerializeField] private bool _isOnRecallPlate = false;
+    
+    [HideInInspector] public bool isOnRecallPlate
+    {
+        get
+        {
+            return _isOnRecallPlate;
+        }
+
+        set
+        {
+            _isOnRecallPlate = value;
+        }
+    }
 
     [SerializeField] private LineRenderer lifeline;
 
@@ -20,6 +35,8 @@ public class DistanceController : MonoBehaviour
     {
         _lightRange = light.range;
         lifeline.positionCount = 2;
+
+        playerInput.actions["Teleportation"].performed += _ => TryTeleport();
     }
 
     private void Update()
@@ -27,9 +44,9 @@ public class DistanceController : MonoBehaviour
         lifeline.SetPosition(0, transform.position);
         lifeline.SetPosition(1, crystal.position);
         float distance = Mathf.Abs(Vector2.Distance(transform.position, crystal.position));
-        float scale = isOnRecallPlate ? recallPlateDieDistanceScale : 1f;
+        float scale = _isOnRecallPlate ? recallPlateDieDistanceScale : 1f;
         
-        Debug.Log(distance+" "+ (dieDistance * scale));
+        //Debug.Log(distance+" "+ (dieDistance * scale));
         
         if(distance > dieDistance * scale)
             RespawnManager.Instance.RespawnPlayer();
@@ -42,5 +59,12 @@ public class DistanceController : MonoBehaviour
                 break;
             }
         }
+    }
+
+    private void TryTeleport()
+    {
+        if (!_isOnRecallPlate) return;
+
+        transform.position = crystal.position;
     }
 }
