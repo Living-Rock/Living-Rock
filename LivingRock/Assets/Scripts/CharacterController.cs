8b0c89429f1e39ccee4bfefd4a8d1d1f46e6754a
VoHeLi <dahrusted@gmail.com>
2022-11-19 08:50:14 +0100

    Meilleur mouvement

--- LivingRock/Assets/Scripts/CharacterController.cs
+++ LivingRock/Assets/Scripts/CharacterController.cs
@@ -10,8 +10,29 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private float playerSpeed;
     [SerializeField] private float attack = 0.1f;
     [SerializeField] private float release = 0.5f;
+    [SerializeField] private float deadZone = 0.1f;
 
-    private Vector2 velocity;
+    private Vector2 velocity = Vector2.zero;
+
+
+    private void UpdateVelocity(Vector2 moveVector)
+    {
+        if (moveVector.sqrMagnitude < deadZone * deadZone)
+        {
+            float velocityNorm = Mathf.Clamp(velocity.magnitude - (playerSpeed / release) * Time.deltaTime, 0.0f, playerSpeed);
+
+            velocity = velocityNorm * velocity.normalized;
+
+            return;
+        }
+       
+        velocity += moveVector * Time.deltaTime * (playerSpeed / attack);
+
+        if (velocity.sqrMagnitude > playerSpeed * playerSpeed)
+        {
+                velocity = velocity.normalized * playerSpeed;
+        }
+    }
 
     void Update()
     {
@@ -19,6 +40,17 @@ public class CharacterController : MonoBehaviour
 
         Vector2 moveVector = playerInput.actions["Move"].ReadValue<Vector2>();
 
-        transform.position += (Vector3)(moveVector * playerSpeed * Time.deltaTime);
+
+        if(moveVector.sqrMagnitude > 1)
+        {
+            moveVector = moveVector.normalized;
+        }
+
+
+        UpdateVelocity(moveVector);
+
+        
+
+        transform.position += (Vector3)(velocity * Time.deltaTime);
     }
 }
